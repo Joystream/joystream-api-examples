b4ac530e3700a89b55f0b41b413df8fd3555144b
bwhm <martin@jsgenesis.com>
2020-06-13 07:14:06 +0200

    adding the missed file

---
+++ src/tokenomics/pool-changes.ts
@@ -0,0 +1,72 @@
+import { PoolChange } from "./interfaces"
+
+export const topUps:PoolChange[] = [
+  {
+    "blockHeight": 909252,
+    "amount": 0,
+    "change": 0
+  },
+  {
+    "blockHeight": 909253,
+    "amount": 0,
+    "change": 2500
+  },
+  {
+    "blockHeight": 1020520,
+    "amount": 0,
+    "change": 420
+  },
+  {
+    "blockHeight": 1138912,
+    "amount": 0,
+    "change": 250
+  },
+  {
+    "blockHeight": 1152762,
+    "amount": 0,
+    "change": 110
+  },
+  {
+    "blockHeight": 1181755,
+    "amount": 0,
+    "change": 120
+  }
+]
+export const oldExchanges: PoolChange[] = [
+    {
+      "amount": 1,
+      "blockHeight": 909276
+    },
+    {
+      "amount": 2000,
+      "blockHeight": 992663
+    },
+    {
+      "amount": 100000,
+      "blockHeight": 1026037
+    },
+    {
+      "amount": 500000,
+      "blockHeight": 1035084
+    },
+    {
+      "amount": 526315,
+      "blockHeight": 1035346
+    },
+    {
+      "amount": 500000,
+      "blockHeight": 1115996,
+    },
+    {
+      "amount": 500000,
+      "blockHeight": 1118640,
+    },
+    {
+      "amount": 1000000,
+      "blockHeight": 1143233
+    },
+    {
+      "amount": 500000,
+      "blockHeight": 1162361
+    }
+  ]

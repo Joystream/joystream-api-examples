65cb9b1296fb7f7f7c9f2590d5465f7e0d8837b0
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-07-23 11:49:09 +0300

    read proper package version

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ exportedScripts.exportDataDirectory = require('./export-data-directory.js')
 exportedScripts.injectDataObjects = require('./inject-data-objects.js')
 
 // This scripts uses the joy injected types introduced in @joystream/types v0.12.0
-if (typesVersion === '0.12.0') {
+if (typesVersion.version === '0.12.0') {
   exportedScripts.listDataDirectory = require('./list-data-directory.js')
 }
 
